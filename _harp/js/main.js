6bed0b8e4c5387c042032ec33faa2238b38663b5
RFTB <preston@rideforthebrand.net>
2014-07-01 19:06:19 -0500

    a

--- _harp/js/main.js
+++ _harp/js/main.js
@@ -1,6 +1,8 @@
 jQuery( document ).ready(function($) {
   $('ol.timeline').find('a').on('click', function(e){
     e.preventDefault();
+    $('ol.timeline').find('a').removeClass('active');
+    $(this).addClass('active');
     $( "#details" ).load( $(this).attr('href'), function() {
       console.log('ok');
     });
