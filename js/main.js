28ed66bf4ed8abbf8e1d81637178b981ff46816f
RFTB <preston@rideforthebrand.net>
2014-07-02 08:43:04 -0500

    h

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 jQuery( document ).ready(function($) {
+
+  var viewport = {
+        'w' : $( window ).width(),
+        'h' : $( window ).height()
+        };
+
+  $('.scroll').height(viewport.h-40);
+
   $('ol.timeline').find('a').on('click', function(e){
     e.preventDefault();
     $('ol.timeline').find('a').removeClass('active');
